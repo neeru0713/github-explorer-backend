79c2a1a4ee6ea8e87c8f88ea410ea02c20272c5b
neeru0713 <neerurani1307@gmail.com>
2024-03-25 23:09:31 +0530

    \username, location, and any other relevant user details.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,19 @@
 const { User } = require("../models/User");
-const { GITHUB_TOKEN } = require('../config/config.js');
+const { GITHUB_TOKEN } = require("../config/config.js");
 const express = require("express");
 const router = express.Router();
 
 router.get("/save-user/:username", async (req, res) => {
   const username = req.params.username;
 
-  const userExists = await User.findOne({ userName: username });
+  const userExists = await User.findOne({ username });
   console.log(userExists);
   if (userExists) {
     res.status(200).json({ message: "User alerady exists" });
   } else {
     let response = await fetch(`https://api.github.com/users/${username}`);
     response = await response.json();
-    response.userName = username;
+    response.username = username;
     const user = new User(response);
     const userDetails = await user.save();
     console.log(userDetails);
@@ -23,7 +23,7 @@ router.get("/save-user/:username", async (req, res) => {
 
 router.get("/find-mutual-followers/:username", async (req, res) => {
   const username = req.params.username;
-  const user = await User.findOne({ userName: username });
+  const user = await User.findOne({ username });
   user.friends = [];
   let savedUser = user;
   await user.save();
@@ -36,19 +36,20 @@ router.get("/find-mutual-followers/:username", async (req, res) => {
     `https://api.github.com/users/${username}/followers`,
     { headers }
   );
-  response = await response.json();
 
-  const promises = response.map(async (item) => {
+  response = await response?.json();
+
+  const promises = response?.map(async (item) => {
     const isFollowingRes = await fetch(
       `https://api.github.com/users/${username}/following/${item.login}`,
       { headers }
     );
     if (isFollowingRes.status === 204) {
-      item.userName = item.login;
+      item.username = item.login;
       user.friends.push(item);
     } else if (isFollowingRes.status === 404) {
       user.friends = user.friends.filter(
-        (friend) => friend.userName !== item.login
+        (friend) => friend.username !== item.login
       );
     }
   });
@@ -57,6 +58,26 @@ router.get("/find-mutual-followers/:username", async (req, res) => {
   savedUser = await user.save();
 
   return res.status(200).json({ user: savedUser });
+});
+
+router.get("/search-users", async (req, res) => {
+  try {
+    const searchQueryKeys = Object.keys(req.query);
+
+    const searchQuery = {};
+
+    searchQueryKeys.forEach((item) => {
+      const val = req.query[item];
+      searchQuery[item] = val;
+    });
+    // myobj.name
+    // myobj[item]
+    const searchResults = await User.find(searchQuery);
+    res.status(200).json({ users: searchResults });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
