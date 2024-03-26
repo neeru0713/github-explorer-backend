1158f381f197f14f258f73d155b0a393c2ca3d5a
neeru0713 <neerurani1307@gmail.com>
2024-03-26 21:59:42 +0530

    Endpoint: /list-users Method: GET Functionality: Return a list of all users from the database. Support sorting by fields like public_repos, followers, etc.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -91,20 +91,42 @@ router.delete("/delete-user/:username", async (req, res) => {
 router.patch("/update-user/:username", async (req, res) => {
   try {
     const username = req.params.username;
-    console.log(username)
+    console.log(username);
     const updates = req.body;
-    console.log(req.body)
+    console.log(req.body);
     const updatedUser = await User.findOneAndUpdate(
       { username: username },
       { $set: updates },
       { new: true }
     );
-    console.log(updatedUser)
+    console.log(updatedUser);
     if (!updatedUser) {
       return res.status(404).json({ error: "User not found" });
     }
 
     return res.status(200).json({ user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/list-users", async (req, res) => {
+  try {
+    let users = await User.find({isDeleted: false});
+    const fieldUsedForSorting = req.query.sortBy;
+
+    if (fieldUsedForSorting) {
+        console.log("Before sorting : ", users)
+
+      users = users.sort((a, b) => {
+        if (a[fieldUsedForSorting] < b[fieldUsedForSorting]) return 1;
+        if (a[fieldUsedForSorting] > b[fieldUsedForSorting]) return -1;
+      });
+
+      console.log("After sorting : ", users)
+    }
+    return res.status(200).json({ users });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal Server Error" });
